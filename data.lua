088074d0596f40da648ecc633cc031bfbecefd87
U-WHGC\ted <ted@ted-E7470.well.ox.ac.uk>
2018-11-13 17:12:16 +0000

    Write functions for preparing data for training

--- data.lua
+++ data.lua
@@ -2,74 +2,115 @@ require "torch"
 
 require "math"
 
+cnp_file
+
 chrom_extract=function(cnp,chrom)
-	return cnp[{{50*(chrom-1)+1,50*chrom},{}}];
+	if chrom<=2 then
+		return torch.zeros(nfeats,chrom_width,1);
+
+	return cnp[{},{chrom_width*(chrom-3)+1,chrom_width*(chrom-2)},{}}];
 end
 
+CNV_action=torch.Tensor({{1,0},{-1,0},{1,-1},{0,1},{0,-1}})
 
-Rewarding=function(ChrA,FocA,StartL,EndL,StartS,EndS)
-	local reward;
-	if ChrA==1 then
-		reward=torch.sum(torch.abs(StartS-1))*log(c/b);
-		reward=reward-ValueNet:forward(EndS);
-	else 
-		if chrA==2 then
-			reward=torch.sum(torch.abs(StartS-2*EndS))*log(b/c);
-			reward=reward+log(WGD);
-		else:
-			reward=log(c/(b+log(EndL-StartL)));
-			if torch.abs(EndL-StartL-25)<2 then
-				reward=log(HC);
-			else 
-				if torch.abs(EndL-StartL-50)<3 then
-					reward=log(WC);
+
+LoadData=function(flag)
+	--when flag is true, load data from file
+	--otherwise use the next state as input
+	if flag then
+		cnp={}
+		cnpfile = io.open(cnp_file,"r")
+		if cnpfile then
+			local counter=0;
+			local x=torch.floor(torch.rand(1)*10)[1]
+			for cnpline in cnpfile:lines() do
+				if((math.floor(counter/2))%10==x) then
+					table.insert(cnp,cnpline:split("\t"));
 				end
-			end
-		end
-	end
-	return reward;
-end
-
-
-	b=1
-	c=0.1
-	WGD=0.2
-	WC=0.04
-	HC=0.05
-
-LoadData=function()
+			counter=counter+1;
+			counter=counter%10;
+            end
+		train.state=torch.Tensor(cnp);
+		cnp=nil;
+		train.state:resize(train.state:size(1)/2,2,train.state:size(2),1);
+	else:
+		train.state=train.next;
+    end
+	
+	
 	local temp
-	temp,train.ChromAct=torch.max(ChromNet:forward(train.state),2)
-	train.focus=torch.Tensor(train.state:size(1),50)
-	train.focus:fill(1);
-	for i=1,train.state:size(1) do
-		if(train.ChromAct[i]>2) then
-			train.focus[i]=chrom_extract(train.state[i],train.ChromAct[i]-2)
+	--temp,train.ChrA=torch.max(ChromNet:forward(train.state),2)
+	--sample chromosome
+	--Extract the cnp for the chromosome
+	Chrom_Model:forward(train.state)
+	train.ChrA=torch.Tensor(train.state:size(1));
+	train.chrom_state=torch.Tensor(train.state:size(1),nfeats,chrom_width,1);
+	for i=1,train.ChrA:size(1) do
+		temp=torch.rand(1)[1];
+		train.ChrA[i]=0;
+		while temp>0 do
+			train.ChrA[i]=train.ChrA[i]+1;
+			temp=temp-Chrom_Model.output[i][train.ChrA[i]];
+		end
+		train.chrom_state[i]=chrom_extract(train.state[i],train.ChrA[i])
+	end
+	
+	--sample cnv
+	--prepare potential cnp
+	CNV_Model:forward(train.state,train.chrom_state)
+	train.CNV=torch.Tensor(train.state:size(1));
+	train.StartL=torch.Tensor(train.state:size(1));
+	train.chrom_state_new=torch.Tensor(train.state:size(1),nfeats,chrom_width,1);
+	local cnv_a;
+	for i=1,train.CNV:size(1)do
+		train.CNV[i]=0;
+		train.StartL[i]=0;
+		train.chrom_state_new[i]=train.chrom_state[i];
+		if train.ChrA[i]>2 then
+			temp=torch.rand(1)[1];
+			while temp>0 do
+				train.CNV[i]=train.CNV[i]+1;
+				temp=temp-CNV_Model.output[i][train.CNV[i]];
+			end
+			train.StartL[i]=math.floor(train.CNV[i]/CNV_action:size(1))+1;
+			cnv_a=train.CNV[i]%CNV_action:size(1);
+			for j=train.StartL[i],chrom_width do
+				train.chrom_state_new[i][1][j][1]=train.chrom_state_new[i][1][j][1]+cnv_a[1]
+				train.chrom_state_new[i][2][j][1]=train.chrom_state_new[i][2][j][1]+cnv_a[2]
 		end
 	end
-	temp,train.FocusAct=torch.max(UpperPolicyNet:forward({train.state,train.focus}),2)
-	train.FocusAct=train.FocusAct+2
-	train.start=(train.ChromAct-3)*50+(train.FocusAct-2)%5+1
-	for i=1,train.state:size(1) do
-		train.end[i]=(train.ChromAct-2)*50
-		while (torch.all(torch.ne(train.state[train.end[i]],train.state[train.start[i]]))) do
-			train.end[i]=train.end[i]-1
-		end
-	end
-	CNV_action=torch.Tensor({{1,0},{-1,0},{1,-1},{0,1},{0-1}})
-	for i=1,train.state:size(1) do
-		if train.ChromAct[i]==1 then
-			train.next[i]=train.state[i]
-		else 
-			if train.ChromAct[i]==2 then
-				train.next[i]=(train.state[i]/2):int()
-			else 
-				train.next[i]=(train.state[i]):clone()
-				train.next[i][train.start[i]:train.end[i]]=train.next[i][train.start[i]:train.end[i]]+CNV_action[train.FocusAct%5]
+	
+	--sample end point
+	End_Model:forward(train.chrom_state,train.chrom_state_new)
+	train.End=torch.Tensor(train.state:size(1));
+	for i=1,train.End:size(1)do
+		train.End[i]=0;
+		if train.ChrA[i]>2 then
+			temp=torch.rand(1)[1];
+			while temp>0 do
+				train.End[i]=train.End[i]+1;
+				temp=temp-End_Point_Model.output[i][train.End[i]];
 			end
 		end
 	end
-	for i=1,train.state:size(1) do
-		train.reward[i]=Rewarding(train.ChromAct[i],train.FocusAct[i],train.start[i],train.end[i],train.state[i],train.next[i])
-	end
+	
+	--Compute CNP at the next time point
+	--Compute Reward-to-go and Advantage
+	train.next=train.state:clone();
+	train.Reward=train.Tensor(train.state:size(1));
+	local startL,endL;
+	for i=1,train.Reward:size(1) do
+		if train.ChrA[i]==2 then
+			train.next[i]=(train.next[i]/2):floor()
+		elseif train.Reward>2 then
+			startL=chrom_width*(ChrA[i]-3)+train.StartL[i];
+			endL=chrom_width*(ChrA[i]-3)+train.End[i];
+			cnv_a=train.CNV[i]%CNV_action:size(1);
+			for j=startL,endL do
+				train.next[i][1][j][1]=train.next[i][1][j][1]+cnv_a[1];
+				train.next[i][2][j][1]=train.next[i][1][j][1]+cnv_a[2];
+		train.Reward[i]=Reward(train.ChrA[i],startL,endL,train.state[i],train.next[i])
+	
+	train.Advantage=Tensor(train.state:size(1));
+	Advantage_cal();
 end
