8293dab21bebc1305680c355e4d13bdf1f6b302a
Yun Feng <yun.feng@well.ox.ac.uk>
2018-11-19 17:28:58 +0000

    reduce linear layer

--- Value.lua
+++ Value.lua
@@ -36,7 +36,7 @@ nchannel = math.floor((math.floor((width)/4.0))/4.0)
 ValueNet:add(nn.Reshape(nkernels[3]*nchannel))
 ValueNet:add(nn.Linear(nkernels[3]*nchannel, nkernels[4]))
 ValueNet:add(nn.SoftMax())
-ValueNet:add(nn.Linear(nkernels[4], nkernels[4]))
+--ValueNet:add(nn.Linear(nkernels[4], nkernels[4]))
 ValueNet:add(nn.Linear(nkernels[4] , 1))
 
 ValueNet_eval=ValueNet:clone()
