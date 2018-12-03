1a8d2a5885dc66bd58b43dc2eeb53bb8daf95329
Yun Feng <yun.feng@well.ox.ac.uk>
2018-12-03 13:17:42 +0000

    revert test

---
+++ Test.lua
@@ -0,0 +1,35 @@
+##test the behavior of networks
+
+require "torch"
+require "nn"
+require "nngraph"
+
+wkdir="/data/ted/WGD/"
+opt={}
+train={}
+
+dofile (wkdir.."Value.lua");
+dofile (wkdir.."Policy.lua");
+--dofile (wkdir.."Advantage.lua");
+--dofile (wkdir.."train.lua");
+--dofile (wkdir.."data.lua");
+
+ValueNet=torch.load(wkdir.."Model_ValueNet");
+ValueNet_eval=torch.load(wkdir.."Model_ValueNet_eval");
+Chrom_Model=torch.load(wkdir.."Model_Chrom_Model");
+CNV_Model=torch.load(wkdir.."Model_CNV_Model");
+End_Point_Model=torch.load(wkdir.."Model_End_Point_Model");
+
+dofile (wkdir.."Advantage.lua");
+dofile (wkdir.."train.lua");
+dofile (wkdir.."data.lua");
+
+train.next=torch.ones(1,2,22*50,1)
+flag=false
+LoadData(flag)
+
+train.next=torch.ones(1,2,22*50,1)
+for i =1,10 do
+	train.next[1][1][i][1]=0
+end
+LoadData(flag)
--- main.lua
+++ main.lua
@@ -21,12 +21,26 @@ counter=0;
 for c=0,cycle do
 	counter=counter+1;
 	if c%10==0 then
-		flag=1;
-        	print(string.format("Start cycle %d", c));
+		if torch.rand(1)[1]>0.5 then
+			flag=1;
+		else 
+			flag=0;
+		end
+		if(counter<1000) then
+			flag2=1;
+			opt.State_Val_eval.learnningRate=1;
+		else
+			flag2=false;
+			opt.State_Val_eval.learningRate=0.01;
+        	end
+		if torch.rand(1)[1]<0.2 then
+			flag2=1;
+		end	
+		print(string.format("Start cycle %d", c));
         	print("Loading data");
-		LoadData(flag);
+		LoadData(flag,flag2);
 	        print("Start train");
-		if counter<100 then
+		if counter<1000 then
 			Val_train();
 		else
 			model_train();
@@ -41,7 +55,8 @@ for c=0,cycle do
 		torch.save(wkdir.."Model_End_Point_Model",End_Point_Model);
 	else
 		flag=false;
-		LoadData(flag);
+		flag2=false;
+		LoadData(flag,flag2);
 		Val_train();
 		torch.save(wkdir.."Model_ValueNet",ValueNet);
 		torch.save(wkdir.."Model_ValueNet_eval",ValueNet_eval);
@@ -51,8 +66,9 @@ for c=0,cycle do
 	end
 	if c%100==0 then
 		flag=false;
-		train.next=torch.ones(1,2,22*50,1)
-		LoadData(flag);
+		flag2=1;
+		train.next=torch.ones(100,2,22*50,1)
+		LoadData(flag,flag2);
 		model_train();
 	end
 end
