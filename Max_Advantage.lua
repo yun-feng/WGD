f322258087f242f88997a0c416d1ad2f9b96c52a
Yun Feng <yun.feng@well.ox.ac.uk>
2019-01-28 13:38:36 +0000

    file for max q-learning

---
+++ Max_Advantage.lua
@@ -0,0 +1,51 @@
+require "torch"
+require "nn"
+require "math"
+
+normal_const=5e-6;
+single_loci_loss=normal_const*(1-2e-1);
+Half_Chromosome_CNV=normal_const*(1-0.4);
+Whole_Chromosome_CNV=normal_const*(1-0.5);
+WGD=normal_const*0.6;
+const1=normal_const*(1-1e-1);
+const2=5;
+
+Reward=function(ChrA,StartL,EndL,StartS,EndS)
+	local reward;
+	if ChrA==2 then
+		reward=torch.sum(torch.abs(StartS-2*EndS))*math.log(single_loci_loss);
+		reward=reward+math.log(WGD);
+	else
+		reward=math.log(const1/(const2+math.log(EndL-StartL+1)));
+		if torch.abs(EndL-StartL+1-chrom_width/2)<2 then
+			reward=math.log(Half_Chromosome_CNV);
+		else 
+			if torch.abs(EndL-StartL+1-chrom_width)<3 then
+				reward=math.log(Whole_Chromosome_CNV);
+			end
+		end
+	end
+	reward=reward+torch.sum(torch.abs(EndS-1))*math.log(single_loci_loss);
+	reward=reward-torch.sum(torch.abs(StartS-1))*math.log(single_loci_loss);
+	return reward;
+end
+
+Advantage_cal=function()
+	Chrom_Model:forward(train.next)
+	train.Advantage=train.Reward-(Chrom_Model.output:select(2,1))
+	train.Advantage=train.Advantage+((Chrom_Model.output):max(2))
+	Chrom_Model:forward(train.state)	
+	train.Advantage=train.Advantage+(Chrom_Model.output:select(2,1))
+
+	CNV_Model:forward({train.state,train.chrom_state})
+	End_Point_Model:forward({train.chrom_state,train.chrom_state_new})
+	for i = 1,train.state:size(1) do
+		train.Advantage[i]=train.Advantage[i]-(Chrom_Model.output[i][train.ChrA[i]])
+		if train.ChrA[i]>2 then
+			train.Advantage[i]=train.Advantage[i]-(CNV_Model.output[i][train.CNV[i]])
+			train.Advantage[i]=train.Advantage[i]-(End_Point_Model.output[i][train.End[i]])
+			train.Advantage[i]=train.Advantage[i]+((End_Point_Model.output[{i,{train.StartL[i],chrom_width}}]):max())
+		end
+	end
+	return train.Advantage;
+end
---
+++ Max_Policy.lua
@@ -0,0 +1,82 @@
+require 'torch'
+require 'nn'
+require 'nngraph'
+
+nfeats = 2
+width = 22*50
+height = 1
+ninputs = nfeats*width*height
+--nkernels = {320,480,960,100}
+chrom_width=width/22;
+nkernels = {160,240,480,22+2}
+
+
+--determin which chromosome to change
+Chrom_Net = nn.Sequential()
+
+
+Chrom_Net:add(nn.SpatialConvolution(nfeats, nkernels[1], 1, 7, 1, 1, 0,3))
+Chrom_Net:add(nn.Threshold(0, 1e-6))
+Chrom_Net:add(nn.SpatialMaxPooling(1,4,1,4,0,2))
+--Chrom_Net:add(nn.Dropout(0.2))
+
+Chrom_Net:add(nn.SpatialConvolution(nkernels[1], nkernels[2], 1, 8, 1, 1, 0,3))
+Chrom_Net:add(nn.Threshold(0, 1e-6))
+Chrom_Net:add(nn.SpatialMaxPooling(1,4,1,4,0,2))
+--Chrom_Net:add(nn.Dropout(0.2))
+
+Chrom_Net:add(nn.SpatialConvolution(nkernels[2], nkernels[3], 1, 8, 1, 1, 0,3))
+Chrom_Net:add(nn.Threshold(0, 1e-6))
+---Chrom_Net:add(nn.Dropout(0.5))
+
+nchannel = math.floor((math.floor((width)/4.0))/4.0)
+Chrom_Net:add(nn.Reshape(nkernels[3]*nchannel))
+Chrom_Net:add(nn.Linear(nkernels[3]*nchannel, nkernels[4]))
+Chrom_Net:add(nn.Threshold(0, 1e-6))
+Chrom_Net:add(nn.Linear(nkernels[4] ,nkernels[4]))
+--Chrom_Net:add(nn.SoftMax())
+
+Chrom_Model=Chrom_Net;
+
+--determine the starting position and type of CNV
+CNV_i1 =-nn.SpatialConvolution(nfeats,nkernels[1]/4, 1, 7, 1, 1, 0,3)
+CNV_h1 =CNV_i1-nn.Threshold(0, 1e-6)
+CNV_h1 =CNV_h1-nn.SpatialMaxPooling(1,20,1,20,0,2)
+
+CNV_i2 =-nn.SpatialConvolution(nfeats,nkernels[1]/4, 1, 7, 1, 1, 0,3)
+CNV_h2 =CNV_i2-nn.Threshold(0, 1e-6)
+
+CNV_Net={CNV_h1,CNV_h2}-nn.JoinTable(2,3)
+
+CNV_Net =CNV_Net-nn.SpatialConvolution(nkernels[1]/4, nkernels[2]/4, 1, 8, 1, 1, 0,3)
+CNV_Net =CNV_Net-nn.Threshold(0, 1e-6)
+CNV_Net =CNV_Net-nn.SpatialMaxPooling(1,4,1,4,0,2)
+
+
+CNV_Net =CNV_Net-nn.SpatialConvolution(nkernels[2]/4, nkernels[3]/4, 1, 8, 1, 1, 0,3)
+CNV_Net =CNV_Net-nn.Threshold(0, 1e-6)
+
+nchannel = math.floor((math.floor((width)/20)+chrom_width)/4)
+CNV_Net =CNV_Net-nn.Reshape(nkernels[3]/4*nchannel)
+CNV_Net =CNV_Net-nn.Linear(nkernels[3]/4*nchannel,4*chrom_width)
+
+CNV_Model=nn.gModule({CNV_i1,CNV_i2},{CNV_Net});
+
+--determin the end position
+End_Point_i1=-nn.Identity()
+End_Point_i2=-nn.Identity()
+End_Point_Net={End_Point_i1,End_Point_i2}-nn.JoinTable(1,3)
+End_Point_Net=End_Point_Net-nn.SpatialConvolution(2*nfeats, nkernels[1]/4, 1, 7, 1, 1, 0,3)
+End_Point_Net =End_Point_Net-nn.Threshold(0, 1e-6)
+--End_Point_Net =End_Point_Net-nn.SpatialMaxPooling(1,4,1,4,0,4-width%4)
+
+
+End_Point_Net =End_Point_Net-nn.SpatialConvolution(nkernels[1]/4, nkernels[2]/4, 1, 7, 1, 1, 0,3)
+End_Point_Net =End_Point_Net-nn.Threshold(0, 1e-6)
+--CNV_Net =CNV_Net-nn.SpatialMaxPooling(1,5,1,5,0,5-math.ceil(width/20)%5)
+
+
+End_Point_Net =End_Point_Net-nn.Reshape(nkernels[2]/4*chrom_width)
+End_Point_Net =End_Point_Net-nn.Linear(nkernels[2]/4*chrom_width,chrom_width)
+
+End_Point_Model=nn.gModule({End_Point_i1,End_Point_i2},{End_Point_Net});
