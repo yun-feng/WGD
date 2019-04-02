068146cf4b764e6c34fd08ebcc4154c80e9e4c4a
U-WHGC\ted <ted@ted-E7470.well.ox.ac.uk>
2019-04-02 15:58:20 +0100

    full test

---
+++ Max_full_test.lua
@@ -0,0 +1,37 @@
+require "torch"
+require "nn"
+require "nngraph"
+
+wkdir="/data/ted/WGD/Max_"
+opt={}
+train={}
+test={}
+
+dofile (wkdir.."Policy2.lua");
+batch_sample=10
+--dofile (wkdir.."Advantage.lua");
+--dofile (wkdir.."train.lua");
+--dofile (wkdir.."data.lua");
+
+Chrom_Model=torch.load(wkdir.."Model_Chrom_Model_com3");
+CNV_Model=torch.load(wkdir.."Model_CNV_Model_com3");
+End_Point_Model=torch.load(wkdir.."Model_End_Point_Model_com3");
+
+dofile (wkdir.."Advantage2.lua");
+dofile (wkdir.."train2.lua");
+dofile (wkdir.."data_simu2.lua");
+
+train.step_sample=5
+
+train.Chr_sample=torch.zeros(batch_sample,train.step_sample)
+train.CNV_sample=torch.zeros(batch_sample,train.step_sample)
+train.End_sample=torch.zeros(batch_sample,train.step_sample)
+train.WGD_sample=torch.zeros(batch_sample)
+
+LoadData_t(train.step_sample)
+
+test.ChrA=torch.zeros(train.step_sample+5)
+test.CNV=torch.zeros(train.step_sample+5)
+test.End=torch.zeros(train.step_sample+5)
+
+Deconvolute(train.state[2],10)
