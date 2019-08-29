0163faf44a6c83ad3dc2ab4f65d4d9a848d8163a
U-WHGC\ted <ted@ted-E7470.well.ox.ac.uk>
2019-08-29 17:52:05 +0100

    Naive2 metho

--- Max_full_test_res.lua
+++ Max_full_test_res.lua
@@ -77,6 +77,41 @@ for i=1,Nsample do
 	test.Naive[i]=math.min(test.Naive[i],train.step_sample+20)
 end
 
+
+test.Naive2=torch.zeros(Nsample)
+temp_state=train.state:clone()
+for i=1,Nsample do
+	if (torch.sum(train.state[i])/2200>1.7) then
+		for chr=1,44 do
+			temp_allele=torch.floor((chr-1)/22)
+			temp_chr=train.state[i][{temp_allele+1,{(chr-1-temp_allele*22)*50+1,(chr-1-temp_allele*22)*50+50},1}]:clone()
+			temp_chr=temp_chr-torch.floor(temp_chr/2)*2
+			temp_break=0
+			for j=1,50 do
+				if(not (temp_chr[j]==temp_break) )then
+				temp_break=temp[j]
+				test.Naive2[i]=test.Naive2[i]+torch.abs(temp_break)
+				end
+			end
+		end
+		train.state[i]=torch.floor(train.state[i]/2)*2
+	end
+	for chr=1,44 do
+		temp_allele=torch.floor((chr-1)/22)	
+		temp_ave=torch.floor(torch.sum(train.state[i][{temp_allele+1,{(chr-1-temp_allele*22)*50+1,(chr-1-temp_allele*22)*50+50},1}])/50+0.5)
+		temp_break=temp_ave
+		for j=1,50 do
+			if(not (train.state[i][{temp_allele+1,(chr-1-temp_allele*22)*50+j,1}]==temp_break) )then
+			temp_break=train.state[i][{temp_allele+1,(chr-1-temp_allele*22)*50+j,1}]
+			test.Naive2[i]=test.Naive2[i]+torch.abs(temp_break-temp_ave)
+			end
+		end
+		test.Naive2[i]=test.Naive2[i]+torch.abs(temp_ave-1)
+	end
+	test.Naive2[i]=math.min(test.Naive2[i],train.step_sample+20)
+end
+
+train.state=temp_state
 test.truth=torch.zeros(Nsample)
 test.rl=torch.zeros(Nsample)
 for i=1,Nsample do
